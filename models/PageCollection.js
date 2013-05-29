802594cdc2e74cdb5f4b97e0324ccdaa1928c62e
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-05-29 16:36:26 -0400

    FMB-128 page JSON structure handling

--- models/PageCollection.js
+++ models/PageCollection.js
@@ -2,6 +2,7 @@ define(
 ['vpl/collection'],
 function(VplCollection) {
     return VplCollection.extend({
+        model: 'models/PageModel',
         dataSource: 'form',
         parse: function(result, args) {
             return result.form.pages;
