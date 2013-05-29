0458b4a6cd07b86cf8c899e897e8947dacef4aa2
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-05-29 16:33:00 -0400

    FMB-132, FMB-133, FMB-135 section model bootstrapping

--- models/PageModel.js
+++ models/PageModel.js
@@ -1,4 +1,36 @@
-define(['vpl/model'],
-function(VplModel) {
-    return VplModel.extend({});
+define(
+[
+    'vpl/model',
+    'models/PageCollection',
+    'models/SectionCollection',
+    'models/SectionModel', 
+    'models/DisplayRuleModel'
+],
+function(VplModel, PageCollection, SectionCollection, SectionModel, DisplayRuleModel) {
+    return VplModel.extend({
+        idAttribute: 'pageUrl',
+        collection: PageCollection,
+        defaults: {
+            pageTitle: "Title",
+            showPageTitle: true,
+            progressBarTitle: "Progress",
+            sections: new SectionCollection,
+            displayRule: new DisplayRuleModel
+        },
+        setMembers: function(modelData) {
+            var $t = this;
+
+            var sectionCollection = new SectionCollection;
+
+            $.each(modelData[0].get('sections'), function() {
+                sectionCollection.add(new SectionModel(this));
+            });
+
+            $t.set('sections', sectionCollection);
+
+            if(typeof modelData[0].get('displayRule') != DisplayRuleModel) {
+                modelData[0].set('displayRule', new DisplayRuleModel(modelData[0].get('displayRule')));
+            }
+        }
+    });
 });
