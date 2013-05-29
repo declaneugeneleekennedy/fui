c01ca71f0c9d52f733941d35e4a091e1095220ee
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-05-29 16:30:40 -0400

    FMB-134 parsing JSON for section

--- models/SectionCollection.js
+++ models/SectionCollection.js
@@ -5,13 +5,13 @@ function(VplCollection) {
         parse: function(result, args) {
             var sections = [];
 
-            $.each(result.form.pages, function()) {
+            $.each(result.form.pages, function() {
                 if(this.pageUrl == args.pageUrl) {
                     $.each(this.sections, function() {
                         sections.push(this);
                     });
                 }
-            }
+            });
 
             return sections;
         }
