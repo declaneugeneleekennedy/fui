55361211b58773a651caffa358b2a7c173eef31e
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-05-29 16:38:29 -0400

    FMB-127 removals for templating

--- views/FormPageView.js
+++
@@ -1,8 +0,0 @@
-define(
-['vpl/view', 'viewModels/FormPageViewModel'],
-function(VplView, FormPageViewModel) {
-    return VplView.extend({
-        template: '/templates/Form.html',
-        viewModel: new FormPageViewModel
-    });
-});
