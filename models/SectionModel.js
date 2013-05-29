876ea725666c9a6ddbd0355e9086121b70abee11
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-05-29 16:32:02 -0400

    FMB-131, FMB-138, FMB-139 section modelling and base bootstrapping for content

--- models/SectionModel.js
+++ models/SectionModel.js
@@ -1,5 +1,17 @@
 define(
-['vpl/model'],
-function(VplModel) {
-    return VplModel.extend({});
+['jquery', 'vpl/model', 'models/ContentCollection', 'models/ContentModel'],
+function($, VplModel, ContentCollection, ContentModel) {
+    return VplModel.extend({
+        defaults: {},
+        initialize: function(attributes) {
+            var $t = this;
+
+            var contentCollection = new ContentCollection();
+            $.each(attributes.contents, function() {
+                contentCollection.add(new ContentModel(this));
+            });
+
+            $t.set('contents', contentCollection);
+        }
+    });
 });
