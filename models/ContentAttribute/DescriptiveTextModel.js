bbf14b65ea8c96f91f2e43530b31bb6533f5cca0
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-07-03 16:54:26 -0400

    FMB-104 overhaul of the content modelling to simplify attributes and elements

--- models/ContentAttribute/DescriptiveTextModel.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'models/ContentAttributeModel'],
-function($, ContentAttributeModel) {
-    return ContentAttributeModel.extend({
-        defaults: $.extend(ContentAttributeModel.prototype.defaults, {
-            text: '',
-            enableScroll: true
-        })
-    });
-});
--- models/ContentAttribute/IdentityVerificationModel.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'models/ContentAttributeModel'],
-function($, ContentAttributeModel) {
-    return ContentAttributeModel.extend({
-        defaults: $.extend(ContentAttributeModel.prototype.extend, {
-            
-        })
-    });
-});
