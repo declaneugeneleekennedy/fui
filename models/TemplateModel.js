3a848815d3ff19f4c7e8102030c0415fbc122e23
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-07-18 16:04:12 -0400

    FMB-99 updates to pull data together for template and form, and persist between renders

---
+++ models/TemplateModel.js
@@ -0,0 +1,59 @@
+define(['jquery', 'underscore', 'backbone', 'loader'],
+function($, _, Backbone, Loader) {
+    return Backbone.Model.extend({
+        loader: new Loader,
+        queue: {},
+        defaults: {
+            templateId: null,
+            colour: {},
+            sprite: null,
+            headerBackgroundImage: null,
+            headerImage: null,
+            formTitleImage: null,
+            formContactType: null,
+            formContactContent: null,
+            radius: null,
+            headingsFontFamily: null,
+            fontFamily: null,
+            fontSize: null,
+            enable3d: null,
+            btnLabelPrevious: null,
+            btnLabelNext: null,
+            btnLabelSave: null,
+            btnLabelSaveSub: null,
+            btnLabelPrint: null,
+            btnLabelPrintSub: null,
+            btnLabelComplete: null,
+            bulletPointUnicodeCharacter: null
+        },
+        initialize: function() {
+            var $t = this;
+
+            _.each($t.get('templateSettings'), function(value, name) {
+                $t.set(name, value);
+            });
+
+            $t.unset('templateSettings');
+        },
+        getFile: function(url, dataType) {
+            var $t = this;
+
+            return $t.loader.load(url, dataType);
+        },
+        getHtmlFile: function(fileName) {
+            var $t = this;
+
+            return $t.getFile($t.getFileUrl(fileName), 'html');
+        },
+        getBaseUrl: function() {
+            var $t = this;
+
+            return '/templates/' + String('000' + $t.get('templateId')).slice(-3) + '/';
+        },
+        getFileUrl: function(fileName) {
+            var $t = this;
+
+            return $t.getBaseUrl() + fileName;
+        }
+    });
+});
