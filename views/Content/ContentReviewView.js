cc9ac09ac1088f9568a8351509dc208da2336007
Declan Kennedy <declaneugeneleekennedy@gmail.com>
2013-07-24 16:12:28 -0400

    FMB-153 content review widget template

--- views/Content/ContentReviewView.js
+++ views/Content/ContentReviewView.js
@@ -1,6 +1,23 @@
-define(['views/ContentView'],
-function(ContentView) {
+define(['underscore', 'global', 'views/ContentView'],
+function(_, Global, ContentView) {
     return ContentView.extend({
-        templateUrl: 'Content/ContentReview.html'
+        templateUrl: 'Content/ContentReview.html',
+        prepare: function() {
+            var $t = this;
+
+            var models = [];
+
+            _.each($t.model.get('contents'), function(contentId) {
+                var model = Global.get('form').getContents().findWhere({
+                    contentId: contentId
+                });
+
+                if(model) {
+                    models.push(model.toJSON());
+                }
+            });
+
+            $t.model.set('models', models);
+        }
     });
 });
